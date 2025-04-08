d172f24f3c5603e8df59d19772aac891a1bf37cf
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-04-08 17:15:44 +0900

    [level 1] Title: 실패율, Time: 7573.68 ms, Memory: 17.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -0,0 +1,22 @@
+def solution(N, stages):
+    users = [0] * (N + 2)         # 각 스테이지별 도달 사용자 수
+    failure = [0] * (N + 1)       # 각 스테이지별 실패율
+    
+    # stages 배열 순회하면서 users 배열 업데이트
+    for s in stages:
+        for idx in range(1, s + 1):
+            users[idx] += 1
+    
+    # users 배열 순회하면서 실패율 계산
+    for idx in range(1, N + 1):
+        # 도달 유저가 없는 경우
+        if users[idx] == 0:
+            failure[idx] = 0
+        else:
+            failure[idx] = (users[idx] - users[idx+1]) / users[idx]
+
+    # 실패율 내림차순 기준으로 [1, 2, 3, .., N] 배열 정렬
+    answer = list(range(1, N + 1))
+    answer.sort(reverse=True, key=lambda x: failure[x])
+    
+    return answer
