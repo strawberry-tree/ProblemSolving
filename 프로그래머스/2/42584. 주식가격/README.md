f0fabe3fbb54baf8553279b56ef8740cd615271c
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-04-10 16:48:42 +0900

    [level 2] Title: 주식가격, Time: 24.10 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#) 
 
 ### 성능 요약
 
-메모리: 19 MB, 시간: 27.53 ms
+메모리: 18.9 MB, 시간: 24.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 10일 16:20:05
+2025년 04월 10일 16:50:52
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -5,17 +5,17 @@ def solution(prices):
     # prices 순회 - O(N)
     for idx, p in enumerate(prices):
         # 값이 떨어지는 상황
-        while stack and stack[-1][1] > p:
-            d_idx, d_p = stack.pop()
+        while stack and prices[stack[-1]] > p:
+            d_idx = stack.pop()
             # (값이 떨어진 시점 - 원래 시점)
             answer[d_idx] = idx - d_idx
         else:
             # 제일 최근의 시점을 (현재 시점, 현재 가격)으로 저장
-            stack.append((idx, p))
+            stack.append(idx)
     
     # 값이 떨어지지 않은 경우도 처리 - O(N)
     while stack:
-        d_idx, d_p = stack.pop()
+        d_idx = stack.pop()
         answer[d_idx] = idx - d_idx
     
     return answer
