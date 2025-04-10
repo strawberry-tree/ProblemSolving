00f701e1f088eacc7070b894161a5255977878dd
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-04-11 00:15:00 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,19 +1,21 @@
-from collections import deque
-from math import ceil
+import math
 
 def solution(progresses, speeds):
-    queue = deque()
-    answer = []
+    finish = 0
+    result = []
     
-    for p, s in zip(progresses, speeds):
-        queue.append(ceil((100 - p) / s))
-    
-    while queue:
-        k = queue.popleft()
-        count = 1
-        while queue and queue[0] <= k:
-            queue.popleft()
-            count += 1
-        answer.append(count)
+    # 작업별 걸리는 날짜 계산, O(N)
+    for i in range(len(progresses)):
+        time = math.ceil((100 - progresses[i]) / speeds[i])
         
-    return answer
+        # 앞 작업보다 짧게 걸림 -> 앞 작업과 동시에 끝남
+        if time <= finish :
+            result[-1] += 1
+            
+        # 앞 작업보다 길게 걸림 -> 앞 작업보다 늦게 끝남
+        # OR 첫 번째 작업일 때
+        else:
+            finish = time
+            result.append(1)
+            
+    return result
