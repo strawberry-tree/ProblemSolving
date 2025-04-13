811fabadd775e90bfbc1652d8e4b2c6c6052c902
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-04-13 20:43:05 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+    char sapling[] = "         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |";
+    printf("%s", sapling);
+    return 0;
+}   
+
