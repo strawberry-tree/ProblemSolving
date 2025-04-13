99ced9e064851feb1e068a7b4991f7f7d22129c1
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-04-13 20:11:08 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.c
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 
 int main(void){
-    int submitted[28];
+    int checked[31] = {0};
+    int index;
     int found;
     int count = 0;
 
     for (int i = 0; i < 28; i++){
-        scanf("%d", &submitted[i]);
+        scanf("%d", &index);
+        checked[index] = 1;
     }
 
     for (int num = 1; num < 31; num++){
-        found = 0;
-        for (int i = 0; i < 28; i++){
-            if (submitted[i] == num){
-                found = 1;
-                break;
-            }
-        }
-        if (found == 0){
+        if (checked[num] == 0){
             printf("%d\n", num);
             count++;
-            if (count >= 2){
+            if (count == 2){
                 break;
             }
         }
