9177feb83a88463d826591ab6d347400ac226186
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-04-14 16:44:56 +0900

    Update 미로 탈출.py

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,18 +1,21 @@
 from collections import deque
 
+# 시작위치 찾기
 def find_start(maps):
     # O(N * M)
     for i in range(len(maps)):
         for j in range(len(maps[i])):
             if maps[i][j] == "S":
                 return (i, j)
-            
+
+# 특정 위치에서 목적지까지 최단 거리
 def find_target(maps, sx, sy, target):
     n = len(maps)
     m = len(maps[0])
     dx = [-1, 0, 1, 0]
     dy = [0, -1, 0, 1]
 
+    # 이동시간 저장 용도
     grid = [[-1] * m for _ in range(n)]
     grid[sx][sy] = 0
         
@@ -34,17 +37,12 @@ def find_target(maps, sx, sy, target):
 def solution(maps):
     sx, sy = find_start(maps)
     result_l = find_target(maps, sx, sy, "L")
-    if result_l is not None:
-        l_dist, lx, ly = result_l
-    else:
+    if result_l is None:
+        return -1
+    result_e = find_target(maps, result_l[1], result_l[2], "E")
+    if result_e is None:
         return -1
     
-    result_e = find_target(maps, lx, ly, "E")
-    if result_e is not None:
-        e_dist, ex, ey = result_e
-    else:
-        return -1
-    
-    result = l_dist + e_dist
+    result = result_l[0] + result_e[0]
     return result
                 
