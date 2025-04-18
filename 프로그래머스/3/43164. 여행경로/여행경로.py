9e03e71be3a83a4552234e2b21cfb5e311bafc2a
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-04-19 00:57:30 +0900

    [level 3] Title: 여행경로, Time: 0.04 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,32 +1,34 @@
-from collections import defaultdict, deque
+from collections import defaultdict
 
-def dfs(node, prev, graph, route, num_tickets):
+def dfs(node, prev, graph, route, curr_tickets, num_tickets):
     route.append(node)
-    if prev:
-        graph[prev].remove(node)
+    if prev is not None:
+        curr_tickets[(prev, node)] -= 1
     
-    if len(route) == num_tickets + 1:
+    if num_tickets == 0:
         return True
-    
+
     for adj in graph[node]:
-        if dfs(adj, node, graph, route, num_tickets):
-            return True
+        if curr_tickets[(node, adj)] > 0:
+            if dfs(adj, node, graph, route, curr_tickets, num_tickets - 1):
+                return True
     
     route.pop()
-    if prev:
-        graph[prev].insert(0, node)
+    if prev is not None:
+        curr_tickets[(prev, node)] += 1
+
     return False
     
 def solution(tickets):
     graph = defaultdict(list)
-    num_tickets = 0
+    curr_tickets = defaultdict(int)
     for a, b in tickets:
         graph[a].append(b)
-        num_tickets += 1
-    for key in graph.keys():
-        graph[key].sort()
+        curr_tickets[(a, b)] += 1
+    for key in graph:
+        graph[key] = sorted(graph[key])
     
     route = []
-    dfs("ICN", None, graph, route, num_tickets)    
+    dfs("ICN", None, graph, route, curr_tickets, len(tickets))    
     
     return route
