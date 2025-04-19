978feb20c60ef590e5c4e5e484306948f21308b5
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-04-20 00:20:22 +0900

    [level 2] Title: 모음 사전, Time: 1.83 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,14 +1,19 @@
 def dfs(word, target, index):
+    # word: 현재 노드의 단어
+    # target: 찾는 단어
+    # index: 현재 노드의 인덱스
     if word == target:
         return index, True
-    
-    if len(word) != 5:
-        for letter in "AEIOU":
-            index, flag = dfs(word + letter, target, index + 1)
-            if flag:
-                return index, flag
-        
+
+    if len(word) == 5:
+        return index, False
+
+    for letter in "AEIOU":
+        index, flag = dfs(word + letter, target, index + 1)
+        if flag:
+            return index, True
+
     return index, False
-        
+
 def solution(word):
     return dfs("", word, 0)[0]
