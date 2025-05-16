19fce3dbfab53611349308bcdd325bfc79f81478
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-17 00:31:42 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 180 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,18 +1,25 @@
-import math
-
-def check_prime(x):
-    if x == 1:
-        return False
-    for i in range(2, int(math.sqrt(x)) + 1):
-        if x % i == 0:
-            return False
-    return True
-
 T = int(input())
+nums = []
 
 for _ in range(T):
-    n = int(input())
+    nums.append(int(input()))
+max_n = max(nums)
+
+is_prime = [True] * (max_n + 1)     # 소수인 경우 True, 아니면 False
+is_prime[1] = False                 # 1은 소수가 아님
+
+# 에라토스테네스의 체
+for i in range(2, max_n // 2 + 1):
+    if not is_prime[i]:
+        continue
+    
+    j = 2
+    while i * j <= max_n:
+        is_prime[i * j] = False
+        j += 1
+
+for n in nums:
     for i in range(n // 2, 0, -1):
-        if check_prime(i) and check_prime(n - i):
+        if is_prime[i] and is_prime[n - i]:
             print(i, n - i)
             break
