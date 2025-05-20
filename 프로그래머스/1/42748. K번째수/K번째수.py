1244a910d5fa79ae1df1879ede3957d721906a2d
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-20 17:25:23 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,7 @@
 def solution(array, commands):
     answer = []
-    for (i, j, k) in commands:
-        subarray = array[i-1:j]
-        subarray.sort()
-        answer.append(subarray[k - 1])
+    for i, j, k in commands:
+        slice = array[i - 1 : j]
+        slice.sort()
+        answer.append(slice[k - 1])
     return answer
