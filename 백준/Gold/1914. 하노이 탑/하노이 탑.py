784323e26037c76fb288512973ab8dbaddfadf22
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-21 19:49:20 +0900

    [Gold V] Title: 하노이 탑, Time: 1104 ms, Memory: 107220 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -1,37 +1,19 @@
+def hanoi(n, start, end, sub):
+    count = 0
+    if n >= 2:
+        count += hanoi(n - 1, start, sub, end)
+    log.append((start, end))
+    count += 1
+    if n >= 2:
+        count += hanoi(n - 1, sub, end, start)
+    return count
+
 N = int(input())
-
-# num개의 원판을 A에서 B 위치로 옮김
-def hanoi_moves(num, A, B):
-    count = 0
-    if num >= 1:
-        # 바닥 원판 제외한 num-1개 원판을 A에서 중간 위치로 옮김
-        count += hanoi_moves(num - 1, A, 6 - A - B)
-        # 바닥 원판을 A에서 B로 옮김
-        moves.append((A, B))
-        count += 1
-        # 바닥 원판 제외한 num-1개 원판을 중간 위치에서 B로 옮김
-        count += hanoi_moves(num - 1, 6 - A - B, B)
-    return count
-
-def hanoi(num, A, B):
-    if num in memo:
-        return memo[num]
-    count = 0
-    if num >= 1:
-        # 바닥 원판 제외한 num-1개 원판을 A에서 중간 위치로 옮김
-        count += hanoi(num - 1, A, 6 - A - B)
-        # 바닥 원판을 A에서 B로 옮김
-        count += 1
-        # 바닥 원판 제외한 num-1개 원판을 중간 위치에서 B로 옮김
-        count += hanoi(num - 1, 6 - A - B, B)
-        memo[num] = count
-    return count
+log = []
 
 if N <= 20:
-    moves = []
-    print(hanoi_moves(N, 1, 3))
-    for (a, b) in moves:
-        print(a, b)
+    print(hanoi(N, 1, 3, 2))
+    for start, end in log:
+        print(start, end)
 else:
-    memo = dict()
-    print(hanoi(N, 1, 3))
+    print(2 ** N - 1)
