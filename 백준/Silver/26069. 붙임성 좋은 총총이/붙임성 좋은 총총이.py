fa96e3873b0cf8318be0a95f3a168b586b144a2e
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-21 14:06:10 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -0,0 +1,10 @@
+N = int(input())
+dancing = {"ChongChong"}
+
+for _ in range(N):
+    A, B = input().split()
+    if A in dancing or B in dancing:
+        dancing.add(A)
+        dancing.add(B)
+        
+print(len(dancing))
