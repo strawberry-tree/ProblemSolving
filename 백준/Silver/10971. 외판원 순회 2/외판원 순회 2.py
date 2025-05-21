8db345d5c78068110f66b7eb002d626301d22ddd
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-21 20:26:15 +0900

    [Silver II] Title: 외판원 순회 2, Time: 544 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,35 +1,25 @@
 from itertools import permutations
 
-# O(N)
-def check_total_cost(perm):
-    total_cost = 0
-    
-    # 첫 도시 -> 마지막 도시까지 비용
-    for i in range(len(perm) - 1):
-        cost = graph[perm[i]][perm[i + 1]]
-        if cost == 0:
-            return None
-        total_cost += cost
-        
-    # 마지막 도시 -> 첫 도시로 돌아가는 비용
-    cost = graph[perm[-1]][perm[0]]
-    if cost == 0:
-        return None
-    total_cost += cost
-    return total_cost
-
 N = int(input())
-graph = []
-
+W = []
 for _ in range(N):
-    graph.append(list(map(int, input().split())))
+    W.append(list(map(int, input().split())))
+    
+def calc_cost(route):
+    route = [0, *route, 0]
+    cost = 0
+    for i in range(len(route) - 1):
+        start, end = route[i], route[i + 1]
+        if W[start][end] == 0:
+            return None
+        cost += W[start][end]
+    return cost
 
 answer = float('inf')
-
-# O(N!)
-for perm in permutations(range(N)):
-    perm_cost = check_total_cost(perm)
-    if perm_cost is None:
-        continue
-    answer = min(perm_cost, answer)
+    
+for route in permutations(range(1, N)):
+    result = calc_cost(route)
+    if result:
+        answer = min(answer, result)
+        
 print(answer)
