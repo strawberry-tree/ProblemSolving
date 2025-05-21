96b1eeccca014d8893cbc87edcef3002f2e339e9
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-21 10:30:34 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 84 ms, Memory: 36632 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,32 +1,23 @@
-def get_stars(n):
+def draw_stars(n):
     if n == 3:
-        return set([(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2,0), (2, 1),(2, 2)])
+        return ["***", "* *", "***"]
     
     n_div3 = n // 3
-    before = get_stars(n_div3)
-    result = set()
-    dx = [0, 1, 2, 0, 2, 0, 1, 2]
-    dy = [0, 0, 0, 1, 1, 2, 2, 2]
+    before = draw_stars(n_div3)
+    result = []
     
-    for x, y in before:
-        for i in range(8):
-            nx = x + dx[i] * n_div3
-            ny = y + dy[i] * n_div3
-            result.add((nx, ny))
-            
+    for line in before:
+        result.append(line * 3)
+    
+    for line in before:
+        result.append(line + " " * n_div3 + line)
+        
+    for line in before:
+        result.append(line * 3)
     return result
-
-def draw_stars(n):
-    array = [[False] * n for _ in range(n)]
-    stars = get_stars(n)
-    
-    for i in range(n):
-        for j in range(n):
-            if (i, j) in stars:
-                print("*", end="")
-            else:
-                print(" ", end="")
-        print()
     
 n = int(input())
-draw_stars(n)
+stars = draw_stars(n)
+
+for line in stars:
+    print(line)
