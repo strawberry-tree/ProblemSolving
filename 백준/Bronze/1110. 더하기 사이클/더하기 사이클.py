328b2de4397e1325c276b2090bb324044bff75f2
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-22 11:15:57 +0900

    [Bronze I] Title: 더하기 사이클, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,15 +1,22 @@
 N = input()
 
 def change_num(N):
+    # 앞에서 구한 합의 맨 오른쪽 자리 수
     first_digit = N[-1]
+    
+    # 각 자리의 숫자를 더하고 맨 오른쪽 자리 수
     second_digit = str(sum(int(i) for i in N))[-1]
+    
+    # 이어붙이고, 반환
     result = first_digit + second_digit
     return result
 
-count = 1
 curr = change_num(N)
+count = 1
 
+# 숫자가 동일할 때까지 반복
 while int(curr) != int(N):
    curr = change_num(curr)
    count += 1
+
 print(count)
