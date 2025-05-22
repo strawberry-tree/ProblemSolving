69434bd301d900c774bff9245288a49bfb46efcd
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-22 11:12:10 +0900

    [Silver II] Title: 부분수열의 합, Time: 272 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,26 +1,33 @@
+# 처음엔 부분수열이 연속되는 숫자여야 하는 줄 알았음
+# 그래서 푸는 데 오래 걸림
+
 from itertools import combinations
 
 N, S = map(int, input().split())
 nums = list(map(int, input().split()))
 
 count = 0
-# 0, ..., N-1 중에 M개 선택 (M = 1, 2, ... N)
-for M in range(1, N + 1):
-    for comb in combinations(range(N), M):
-        total = 0
-        for idx in comb:
-            total += nums[idx]
-        if total == S:
-            count += 1
-print(count)
 
+# 현재 합이 curr인 부분수열에 원소를 추가한다
+# 단 인덱스 i 이후 원소만 추가할 수 있다
+def check_sum(i, curr):
+    # 더 추가할 원소가 없음
+    if i >= N:
+        if curr == S:
+            return 1
+        else:
+            return 0
 
-# # 부분수열의 길이
-# for i in range(1, N):
-#     # 부분수열의 첫 인덱스
-#     for j in range(N - i):
-#         length = sum(nums[j:j+i])
-#         if length == S:
-#             count += 1
-        
-# print(count)
+    count = 0
+    
+    # 더 추가 안 하고 멈추는 경우
+    if i > 0:   # 빈 수열은 부분수열이 아님
+        count += check_sum(N, curr)
+    
+    # 더 추가하는 경우
+    for j in range(i, N):    
+        count += check_sum(j + 1, curr + nums[j])
+    
+    return count 
+    
+print(check_sum(0, 0))
