9ababbcb09db80af65a074d6756bfcaf30abe298
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-22 11:13:27 +0900

    [Silver II] Title: 부분수열의 합, Time: 244 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,8 +1,6 @@
 N, S = map(int, input().split())
 nums = list(map(int, input().split()))
 
-count = 0
-
 # 현재 합이 curr인 부분수열에 원소를 추가한다
 # 단 인덱스 i 이후 원소만 추가할 수 있다
 def check_sum(i, curr):
@@ -15,13 +13,13 @@ def check_sum(i, curr):
 
     count = 0
     
-    # 더 추가 안 하고 멈추는 경우
-    if i > 0:   # 빈 수열은 부분수열이 아님
-        count += check_sum(N, curr)
-    
     # 더 추가하는 경우
     for j in range(i, N):    
         count += check_sum(j + 1, curr + nums[j])
+        
+    # 더 추가 안 하고 멈추는 경우
+    if i > 0:   # 빈 수열은 부분수열이 아님
+        count += check_sum(N, curr)
     
     return count 
     
