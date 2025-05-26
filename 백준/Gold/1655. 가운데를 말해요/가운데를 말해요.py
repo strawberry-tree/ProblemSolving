b5687b593bad5f0b0f21f59f0baf78f8a84daa90
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-26 22:39:08 +0900

    [Gold II] Title: 가운데를 말해요, Time: 212 ms, Memory: 39016 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -6,27 +6,30 @@ min_heap = []
 max_heap = []
 
 N = int(input())
-mid_value = -float('inf')
+
 
 for i in range(N):
     num = int(input())
-    
-    
-    if num >= mid_value: # 중앙값 이상일 시
-        heapq.heappush(min_heap, num)
-    
-    
-    else: # 중앙값 미만일 시
-        # 최대 힙은 음수 값
+    if i == 0:
         heapq.heappush(max_heap, -num)
+        mid_value = num
+
+    else:
+        if num >= mid_value: # 중앙값 이상일 시
+            heapq.heappush(min_heap, num)
         
-    # 길이 구하기는 O(1)    
-    if len(min_heap) > len(max_heap):
-        popped = heapq.heappop(min_heap)
-        heapq.heappush(max_heap, -popped)
-    elif len(min_heap) + 1 < len(max_heap):
-        popped = -heapq.heappop(max_heap)
-        heapq.heappush(min_heap, popped)
+        
+        else: # 중앙값 미만일 시
+            # 최대 힙은 음수 값
+            heapq.heappush(max_heap, -num)
+            
+        # 길이 구하기는 O(1)    
+        if len(min_heap) > len(max_heap):
+            popped = heapq.heappop(min_heap)
+            heapq.heappush(max_heap, -popped)
+        elif len(min_heap) + 1 < len(max_heap):
+            popped = -heapq.heappop(max_heap)
+            heapq.heappush(min_heap, popped)
     
     mid_value = -max_heap[0]
     print(mid_value)
