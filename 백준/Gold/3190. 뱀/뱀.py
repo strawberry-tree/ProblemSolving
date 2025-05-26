1eb30fba1101a86fb65d0eff7a2d3c4ec3d9c863
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-26 17:39:26 +0900

    [Gold IV] Title: 뱀, Time: 56 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,15 +1,9 @@
 from collections import deque
 import sys
-
 input = sys.stdin.readline
 
 N = int(input())    # 보드의 크기
 K = int(input())    # 사과의 개수
-
-# 배열의 값: 뱀이 마지막으로 해당 칸을 지난 시간
-# 초기값은 -1
-grid = [[-1] * N for _ in range(N)]
-grid[0][0] = 0
 
 # 사과의 위치
 apples = set()         
@@ -18,64 +12,53 @@ for _ in range(K):
     apples.add((a - 1, b - 1))
     
 L = int(input())    # 뱀의 방향변환 정보
-queue = deque()
+comm_queue = deque() # 모든 방향변환 정보를 담은 큐
 
 # 시간 및 명령
 for _ in range(L):
     time, command = input().split()
-    queue.append((int(time), command))
+    comm_queue.append((int(time), command))
     
 # 게임이 언제 끝나는지 반환
 def gameplay():
-    face = 0        # 우회전이면 +1, 좌회전이면 -1
-    curr_time = 0   # 몇 초?
+    curr_time = 0   # 지금 몇 초?
     head_x = 0      # 머리의 위치 - 몇행?
     head_y = 0      # 머리의 위치 - 몇열?
-    length = 1      # 현재 뱀의 길이
     
-    # 뱀이 이동하는 좌표를 반환
-    def move():
-        # 이동 방향 (순서대로 우 -> 하 -> 좌 -> 상)
-        dx = [0, 1, 0, -1]
-        dy = [1, 0, -1, 0]
-        nx, ny = head_x + dx[face], head_y + dy[face]
-        if nx < 0 or N <= nx or ny < 0 or N <= ny:
-            return None    # 벽에 부딪힘 -> 게임 끝!
-        if grid[nx][ny] >= curr_time - length:
-            return None    # 뱀에 부딪힘 -> 게임 끝!
-        # 해당 칸으로 이동
-        grid[nx][ny] = curr_time
-        return nx, ny
+    # 현재 뱀이 존재하는 칸의 모든 좌표
+    snake_queue = deque([(0, 0)])
+    
+    # 이동 방향 (순서대로 우 -> 하 -> 좌 -> 상)
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    face = 0        # 우회전이면 +1, 좌회전이면 -1
         
     while True:
-        if not queue:
-            set_time = float('inf')
-        else:
-            set_time, change = queue.popleft()
+        curr_time += 1
         
-        # set_time 초 까지 움직임
-        while curr_time < set_time:
-            curr_time += 1
-            move_result = move()
-            if move_result is None:
-                return curr_time
-            head_x, head_y = move_result
-            
-            # 사과가 있는 경우
-            if (head_x, head_y) in apples:
-                apples.remove((head_x, head_y))
-                length += 1
+        # 뱀 위치 이동
+        nx, ny = head_x + dx[face], head_y + dy[face]
+        if nx < 0 or N <= nx or ny < 0 or N <= ny:
+            return curr_time    # 벽에 부딪힘 -> 게임 끝!
+        if (nx, ny) in snake_queue:
+            return curr_time    # 뱀에 부딪힘 -> 게임 끝!
+        snake_queue.append((nx, ny))    # 머리의 위치를 큐에 추가
+        head_x, head_y = nx, ny
+        
+        # 사과가 있는 경우
+        if (head_x, head_y) in apples:
+            apples.remove((head_x, head_y))
+        else:
+            snake_queue.popleft() # 꼬리의 위치를 큐에서 빼기
                     
-        # 방향을 회전한다
-        if change == "L":
-            face -= 1
-            if face < 0:
-                face = 3 
-        elif change == "D":
-            face += 1
-            if face >= 4:
-                face = 0
+        # 시간이 된 경우, 방향을 회전한다
+        if comm_queue and comm_queue[0][0] == curr_time:
+            command = comm_queue.popleft()[1]
+            if command == "L":
+                face = (face - 1) % 4
+            else:
+                face = (face + 1) % 4
                  
-print(gameplay())
+print(gameplay()) 
                 
         
