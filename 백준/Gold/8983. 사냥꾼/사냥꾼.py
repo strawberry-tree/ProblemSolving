e159b6b34eba8aaec2e77e159f36a56f9d81115d
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-27 09:43:50 +0900

    [Gold IV] Title: 사냥꾼, Time: 544 ms, Memory: 32412 KB, Score: 60 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -6,22 +6,17 @@ M, N, L = map(int, input().split())
 
 # 사대의 위치
 sadaes = set(map(int, input().split()))
-
-# 동물의 위치
-dongmuls = []
-for _ in range(N):
-    dongmuls.append(list(map(int, input().split())))
-    
-# 지금 위치일 때, x마리 이상 사냥 가능?
-# def can_fire(x):
     
 count = 0
-for x, y in dongmuls:
+for _ in range(N):
+    x, y = map(int, input().split())
     sadae_left = max(min(sadaes), x - (L - y))
     sadae_right = min(max(sadaes), x + (L - y))
-    can_fire_sadae = set(range(sadae_left, sadae_right + 1))
-    if len(can_fire_sadae & sadaes) > 0:
-        count += 1
+    
+    for s in sadaes:
+        if sadae_left <= s <= sadae_right:
+            count += 1
+            break
         
 print(count)
     
