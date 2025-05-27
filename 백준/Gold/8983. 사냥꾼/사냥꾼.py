b932ac5da3e272b878817db0a511cf9f450fbd63
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-27 09:52:05 +0900

    [Gold IV] Title: 사냥꾼, Time: 440 ms, Memory: 43448 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -5,19 +5,33 @@ input = sys.stdin.readline
 M, N, L = map(int, input().split())
 
 # 사대의 위치
-sadaes = set(map(int, input().split()))
+sadaes = list(map(int, input().split()))
+sadaes.sort()
+
+# 이분탐색: 쏠 수 있는 사대가 있나요?
+def binary_search(fire_a, fire_b): 
+    l = 0
+    r = M - 1
     
+    while l <= r:
+        m = (l + r) // 2
+        if fire_a <= sadaes[m] <= fire_b:
+            return True
+        elif sadaes[m] < fire_a:
+            l = m + 1
+        else:   # fire_b < sadaes[m]
+            r = m - 1
+    return False
+
+# 동물 수 세기
 count = 0
 for _ in range(N):
     x, y = map(int, input().split())
-    sadae_left = max(min(sadaes), x - (L - y))
-    sadae_right = min(max(sadaes), x + (L - y))
+    fire_a = max(sadaes[0], x - (L - y))
+    fire_b = min(sadaes[-1], x + (L - y))
     
-    for s in sadaes:
-        if sadae_left <= s <= sadae_right:
-            count += 1
-            break
+    # 사대의 위치를 이분탐색으로?
+    if binary_search(fire_a, fire_b):
+        count += 1
         
 print(count)
-    
-    
