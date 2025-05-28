5bae2827eedb10568b3d159a23ce2d8574388aff
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-28 20:27:54 +0900

    [Gold IV] Title: 행렬 제곱, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
@@ -1,42 +1,38 @@
-# import sys
-# input = lambda: sys.stdin.readline().rstrip()
+N, times = map(int, input().split())
 
-N, B = map(int, input().split())
-A = []
-
-for _ in range(N):
-    A.append(list(map(int, input().split())))
-
-dp = dict()
-
-def matmul(X, Y):
-    Z = [[0] * N for _ in range(N)]
-
+# 행렬 A, B 곱하기
+def matmul(A, B):
+    result = [[0] * N for _ in range(N)]
     for i in range(N):
-        for j in range(N):
-            result = 0
+        for j in range(N):       
+            # AB의 행렬곱 C의 i행 j열
+            # A의 i행, B의 j열 모든 원소를 서로 곱하고 합
+            value = 0
             for k in range(N):
-                result += X[i][k] * Y[k][j]
-            Z[i][j] = result % 1000
+                value += A[i][k] * B[k][j]
+            result[i][j] = value % 1000
+    return result
 
-    return Z
-
-def matpower(A, B):
-    if B not in dp:
-        if B == 1:
-            dp[B] = A
-        elif B % 2 == 0:
-            C = matpower(A, B // 2)
-            dp[B] = matmul(C, C)
+# 행렬 grid의 times 제곱
+def power(grid, times):
+    # 그냥 grid를 return하면 안 되는 이유: B = 1일 때 나머지처리 안됨
+    if times == 1:
+        return grid
+    else:
+        half = power(grid, times // 2)
+        if times % 2 == 0:
+            return matmul(half, half)
         else:
-            C = matpower(A, B // 2)
-            D = matpower(A, B // 2 + 1)
-            dp[B] = matmul(C, D)
-    return dp[B]
+            return matmul(matmul(half, half), power(grid, 1))
+        
+grid = []
+for _ in range(N):
+    grid.append(list(map(int, input().split())))
 
-
-result = matpower(A, B)
 for i in range(N):
     for j in range(N):
-        print(result[i][j] % 1000, end=" ")
-    print()
+        grid[i][j] = grid[i][j] % 1000
+
+answer = power(grid, times)
+for i in range(N):
+    print(*answer[i])
