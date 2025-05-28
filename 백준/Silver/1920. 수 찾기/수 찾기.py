812e0e2c4b0c689bb889edbbdee11fbcdf9ebec1
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-28 10:37:21 +0900

    [Silver IV] Title: 수 찾기, Time: 288 ms, Memory: 50700 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,27 +1,15 @@
+import bisect
+
 N = int(input())
 A = list(map(int, input().split()))
-A.sort()    # 정렬은 필수!!
+A.sort()
 
 M = int(input())
 targets = list(map(int, input().split()))
 
-# 이진 탐색
-def find(target):
-    l = 0
-    r = N - 1
-    
-    while l <= r:
-        m = (l + r) // 2
-
-        if A[m] == target:
-            return 1
-        elif A[m] < target:
-            l = m + 1
-        else:
-            r = m - 1
-    
-    return 0
-
-
 for target in targets:
-    print(find(target))
+    result = bisect.bisect_right(A, target) - bisect.bisect_left(A, target)
+    if result > 0:
+        print(1)
+    else:
+        print(0)
