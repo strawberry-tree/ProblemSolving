da56bbca448467503019b82f49fdc4666b7adc04
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-29 09:38:13 +0900

    [Silver IV] Title: 큐 2, Time: 1504 ms, Memory: 112516 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,36 +1,33 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
 
+N = int(input())
 queue = deque()
 
-N = int(input())
-
 for _ in range(N):
-    command = input().strip().split()
-    
+    command = input().split()
     if command[0] == "push":
         queue.append(int(command[1]))
     elif command[0] == "pop":
-        if not queue:
-            print(-1)
-        else:
+        if queue:
             print(queue.popleft())
+        else:
+            print(-1)
     elif command[0] == "size":
         print(len(queue))
     elif command[0] == "empty":
-        if not queue:
-            print(1)
-        else:
+        if queue:
             print(0)
+        else:
+            print(1)
     elif command[0] == "front":
-        if not queue:
-            print(-1)
-        else:
+        if queue:
             print(queue[0])
-    elif command[0] == "back":
-        if not queue:
-            print(-1)
         else:
+            print(-1)
+    elif command[0] == "back":
+        if queue:
             print(queue[-1])
+        else:
+            print(-1)
