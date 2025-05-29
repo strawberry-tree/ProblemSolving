ad471bac1c78d204b2a87af73ed7ab81f687dff9
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-05-29 09:35:13 +0900

    [Silver IV] Title: 스택, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -5,26 +5,23 @@ N = int(input())
 stack = []
 
 for _ in range(N):
-    command = input().strip()
-    
-    if command.startswith("push"):
-        _, value = command.split()
-        stack.append(int(value))
-    
-    elif command == "top":
-        if len(stack) <= 0:
-            print(-1)
-        else:
-            print(stack[-1])
-    
-    elif command == "size":
-        print(len(stack))
-        
-    elif command == "empty":
-        print(int(len(stack) == 0))
-        
-    elif command == "pop":
-        if len(stack) <= 0:
-            print(-1)
-        else:
+    command = input().split()
+    if command[0] == "push":
+        stack.append(int(command[1]))
+    elif command[0] == "pop":
+        if stack:
             print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == "size":
+        print(len(stack))
+    elif command[0] == "empty":
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == "top":
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
