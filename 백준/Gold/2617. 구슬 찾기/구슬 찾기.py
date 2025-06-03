85ccceb8805904636a11a984a0e214eab06647b4
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-06-03 20:28:51 +0900

    [Gold IV] Title: 구슬 찾기, Time: 236 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2617. 구슬 찾기/구슬 찾기.py
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.py
@@ -3,29 +3,31 @@ N, M = map(int, input().split())
 input = sys.stdin.readline
 
 INF = float('inf')
-graph = [[INF] * (N + 1) for _ in range(N + 1)]
+graph = [[False] * (N + 1) for _ in range(N + 1)]
 
 for i in range(1, N + 1):
-    graph[i][i] = 0
+    graph[i][i] = False
 
 for _ in range(M):
     heavy, light = map(int, input().split())
-    graph[heavy][light] = 1 
+    graph[heavy][light] = True 
 
 for k in range(1, N + 1):
     for i in range(1, N + 1):
         for j in range(1, N + 1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+            if not graph[i][j]:
+                graph[i][j] = graph[i][k] and graph[k][j]
             
 answer = 0
 for i in range(1, N + 1):
     # 더 가벼운 게 몇 개?
-    row_check = sum(graph[i][j] < INF for j in range(1, N + 1))
+    row_check = sum(graph[i][j] for j in range(1, N + 1))
     
     # 더 무거운 게 몇 개?
-    col_check = sum(graph[j][i] < INF for j in range(1, N + 1))
+    col_check = sum(graph[j][i] for j in range(1, N + 1))
     
-    if row_check >= (N + 3) // 2 or col_check >= (N + 3) // 2:
+    if row_check >= (N + 1) // 2 or col_check >= (N + 1) // 2:
         answer += 1
 
 print(answer)
+    
