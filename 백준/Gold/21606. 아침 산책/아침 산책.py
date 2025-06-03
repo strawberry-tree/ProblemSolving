27da8e22137c0733827cfbd9d2011fa8472ec886
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-06-03 21:46:44 +0900

    [Gold III] Title: 아침 산책, Time: 936 ms, Memory: 69908 KB, Score: 73 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -5,28 +5,33 @@ N = int(input())
 # 1 실내 0 실외
 in_out = [0] + list(map(int, input().strip()))
 graph = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
 count = 0
 
 for _ in range(N - 1):
     u, v = map(int, input().split())
     graph[u].append(v)
+    graph[v].append(u)
 
 def dfs(i, curr):
     global count
+    visited[i] = True
     
     # 실내인 경우
     if in_out[i] == 1:
         count += 2 * len(curr)
         for j in graph[i]:
-            dfs(j, {i})
+            if not visited[j]:
+                dfs(j, {i})
         curr.add(i)
-        return curr 
+        return curr
 
     # 실외인 경우
-    else:
+    else: 
         for j in graph[i]:
-            curr = dfs(j, curr)
-        
-
+            if not visited[j]:
+                curr.update(dfs(j, curr))
+        return curr
+    
 dfs(1, set())
 print(count)
