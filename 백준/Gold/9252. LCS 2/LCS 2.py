e4bb4efe6a20acd1be9e61a3defacd00e2ef2acf
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-06-12 00:31:16 +0900

    [Gold IV] Title: LCS 2, Time: 436 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -4,17 +4,28 @@ str_2 = input()
 N = len(str_1)
 M = len(str_2)
 
-dp = [[""] * (M + 1) for _ in range(N + 1)]
+dp = [[0] * (M + 1) for _ in range(N + 1)]
 
 for i in range(1, N + 1):
     for j in range(1, M + 1):
         if str_1[i - 1] == str_2[j - 1]:
-            dp[i][j] = dp[i - 1][j - 1] + str_1[i - 1]
+            dp[i][j] = dp[i - 1][j - 1] + 1
         else:
-            if len(dp[i - 1][j]) >= len(dp[i][j - 1]):
-                dp[i][j] = dp[i - 1][j]
-            else:
-                dp[i][j] = dp[i][j - 1]
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
                
-print(len(dp[N][M])) 
+i = N
+j = M
+result = []
+
+while i > 0 and j > 0: 
+    if str_1[i - 1] == str_2[j - 1]:
+        result.append(str_1[i-1])
+        i -= 1
+        j -= 1
+    elif dp[i-1][j] > dp[i][j-1]:
+        i -= 1
+    else:
+        j -= 1
+
 print(dp[N][M])
+print("".join(reversed(result)))
