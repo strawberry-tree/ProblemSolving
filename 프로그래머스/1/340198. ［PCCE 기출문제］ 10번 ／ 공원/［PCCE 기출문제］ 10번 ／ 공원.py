6cf600279a640072270509b38df7a3698ba4d340
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-16 19:22:43 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 1.97 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -1,30 +1,24 @@
-def square_possible(park, x, y, mat_side):
-    for dx in range(mat_side):
-        for dy in range(mat_side):
-            if park[x + dx][y + dy] != "-1":
-                return False
-    return True
-    
-def check_len(park, mat_side):
-    mat_x_len = len(park)
-    mat_y_len = len(park[0])
-    for x in range(mat_x_len - mat_side + 1):
-        for y in range(mat_y_len - mat_side + 1):
-            if square_possible(park, x, y, mat_side):
-                return True
-    return False
-    
 def solution(mats, park):
-    answer = 0
-
-    mats.sort(reverse = True)
+    x_len = len(park)
+    y_len = len(park[0])
     
-    for mat_side in mats:
-        if check_len(park, mat_side):
-            return mat_side
+    dp = [[0] * (len(park[0]) + 1) for _ in range(len(park) + 1)]
+    
+    max_side = 0
+    
+    for x in range(1, x_len + 1):
+        for y in range(1, y_len + 1):
+            if park[x-1][y-1] != "-1":
+                dp[x][y] = 0
+            else:
+                dp[x][y] = min(dp[x-1][y-1], dp[x][y-1], dp[x-1][y]) + 1
+                max_side = max(max_side, dp[x][y])
+    
+    print(dp)
+    print(max_side)
+    
+    mats.sort(reverse=True)
+    for m in mats:
+        if m <= max_side:
+            return m
     return -1
-    
-    
-                            
-    
-    
