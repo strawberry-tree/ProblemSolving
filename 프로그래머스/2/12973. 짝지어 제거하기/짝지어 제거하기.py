d972d65c504e0dab2dfc76f42b1a924dbcdd3d1d
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 16:37:41 +0900

    [level 2] Title: 짝지어 제거하기, Time: 91.25 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,12 +1,13 @@
-# 시간복잡도 - O(N)
 def solution(s):
     stack = []
-
-    # 각 문자를 순회 - O(N)
+    
     for letter in s:
-        if not stack or stack[-1] != letter:
-            stack.append(letter)
-        else:
+        if stack and letter == stack[-1]:
             stack.pop()
-
-    return int(not stack)
+        else:
+            stack.append(letter)
+        
+    if stack:
+        return 0
+    else:
+        return 1
