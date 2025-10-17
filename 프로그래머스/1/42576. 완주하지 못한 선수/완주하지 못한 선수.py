163e014838a938d5ee9863f634ebae60bc12e682
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 17:47:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 57.23 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,17 +1,11 @@
+from collections import Counter
+
 def solution(participant, completion):
-    # 사전으로 만들기 - O(N)
-    p_dict = dict()
-    for p in participant:
-        if p in p_dict:
-            p_dict[p] += 1
-        else:
-            p_dict[p] = 1
+    par_set = Counter(participant)
     
-    # 완주한 사람 확인 - O(N)
-    for p in completion:
-        p_dict[p] -= 1
+    for c in completion:
+        par_set[c] -= 1
         
-    # 완주 못 한 사람 찾기 - O(N)
-    for key, value in p_dict.items():
-        if value != 0:
+    for key in par_set:
+        if par_set[key] == 1:
             return key
