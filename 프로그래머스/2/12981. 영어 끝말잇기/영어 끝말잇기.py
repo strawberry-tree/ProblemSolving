42841343bc4cd79666cb81d9bbbcd78fdeae3ed1
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 21:47:01 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.05 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,20 +1,13 @@
 def solution(n, words):
-    used_words = set([words[0]])      # 이미 사용된 단어
+    used_words = set()
     
-    for i in range(1, len(words)):
-        number = i % n + 1          # 몇 번째 사람?
-        order = i // n + 1          # 몇 번째 차례?
+    for i in range(len(words)):
+        word = words[i]
         
-        # 이미 사용한 단어가 아니면 탈락
-        if words[i] in used_words:
-            return [number, order]
-        
-        # 끝말잇기 조건 만족하지 않으면 탈락
-        if words[i][0] != words[i - 1][-1]:
-            return [number, order]
-        
-        # 이미 사용한 단어를 집합에 추가
-        used_words.add(words[i])
-        
-    # 탈락자가 없어
+        if word in used_words:
+            return [i % n + 1, i // n + 1]
+        elif i >= 1 and words[i][0] != words[i-1][-1]:
+            return [i % n + 1, i // n + 1]
+        used_words.add(word)
+            
     return [0, 0]
