9f4297c97fe0dc52f4401096b08d8273b67fe2ad
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 15:54:08 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.45 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,14 +1,11 @@
-from itertools import combinations 
-
 def solution(numbers):
-    # 중복을 허용하지 않음 - 집합
-    answer = set()
+    result_set = set()
     
-    # 두 개의 수를 뽑아 수 더하기
-    for (a, b) in combinations(numbers, 2):
-        answer.add(a + b)
-    
-    # 집합을 리스트로 변환 후, 오름차순으로 정렬
-    answer = list(answer)
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            result = numbers[i] + numbers[j]
+            result_set.add(result)
+            
+    answer = list(result_set)
     answer.sort()
     return answer
