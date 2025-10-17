9fff94b3aac4e1b2968bf23fc27381eb93be7ba0
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 17:55:41 +0900

    [level 2] Title: 할인 행사, Time: 185.27 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,19 +1,15 @@
+from collections import Counter
 def solution(want, number, discount):
-    # 정현이가 원하는 상품 - 최대 10번 연산
-    want_number = dict()
-    for i in range(len(want)):
-        want_number[want[i]] = number[i]
+    def check(dis_counter):
+        for j in range(len(want)):
+            if dis_counter[want[j]] != number[j]:
+                return False
+        return True
+    answer = 0
     
-    # 딕셔너리에서 현재 할인상품 넣으면서 비교 - O(N)
-    result = 0
-    for i, name in enumerate(discount):
-        if name in want_number:
-            want_number[name] -= 1
-        if i >= 10 and discount[i - 10] in want_number:
-            want_number[discount[i - 10]] += 1
-            
-        # want_number의 모든 value가 0 이하일 때 - 최대 10번 연산
-        if all(x <= 0 for x in want_number.values()):
-            result += 1
+    for i in range(len(discount) - 9):
+        dis_counter = Counter(discount[i:i+10])
+        if check(dis_counter):
+            answer += 1
 
-    return result
+    return answer
