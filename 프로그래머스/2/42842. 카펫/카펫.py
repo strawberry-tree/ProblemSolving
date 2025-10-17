9eeaef09a8d0922486536e1642bc6c05a68f6bba
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 20:18:36 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,8 @@
 import math
-
 def solution(brown, yellow):
-    for i in range(1, int(math.sqrt(yellow)) + 1):
-        if yellow % i != 0:
-            continue
-        height = i + 2
-        width = (yellow // i) + 2
-        if (width * height) == (brown + yellow):
-            return [width, height]
+    area = brown + yellow
+    for sero in range(3, int(math.sqrt(area)) + 1):
+        if area % sero == 0:
+            garo = area // sero
+            if (garo - 2) * (sero - 2) == yellow:
+                return [garo, sero]
