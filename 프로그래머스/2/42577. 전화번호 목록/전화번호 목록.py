115a051cc2f74d7fcc33476ff2092642fcf0685a
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 21:56:23 +0900

    [level 2] Title: 전화번호 목록, Time: 465.41 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,13 +1,8 @@
 def solution(phone_book):
-    # O(N log N)
-    phone_book.sort(key = lambda x: len(x))
-    phone_set = set()
+    phone_set = set(phone_book)
     
-    # phone_book의 길이가 N일 때 (O(N))
-    for number in phone_book:
-        # 전화번호의 길이는 최대 20
-        for i in range(len(number)):
-            if number[:i+1] in phone_set:
+    for p in phone_book:
+        for k in range(min(20, len(p))):
+            if p[:k] in phone_set:
                 return False
-        phone_set.add(number)
     return True
