f38791e1cce7b2aaa6dc0482d2fa674cbbe0977f
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 16:30:43 +0900

    [level 2] Title: 괄호 회전하기, Time: 76.82 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,39 +1,28 @@
-# 문자열 회전 (O(N))
-def rotate(s, num):
-    return s[num:] + s[:num]
-
-# 올바른 괄호 문자열? (O(N))
-def check(s):
-    # 괄호 문자열
-    match = {"(": ")", "[": "]", "{": "}"}
-    
-    stack = []
-    for letter in s:
-        if letter in ["(", "[", "{"]:
-            stack.append(letter)
-        else:
-            # 짝을 이룰 여는 괄호가 없는 경우
-            if not stack:
-                return False
-            open = stack.pop()
-            
-            # 닫는 괄호와 여는 괄호가 쌍을 안 이루는 경우
-            if match[open] != letter:
-                return False
-    
-    # 짝을 찾지 못한 여는 괄호가 없어야 함
-    if stack:
-        return False
-    else:
-        return True
-            
 def solution(s):
-    answer = 0
-    
-    # x칸씩 회전 반복 (O(N^2))
-    for x in range(0, len(s)):
-        result = check(rotate(s, x))
-        if result:
-            answer += 1
+    # 올바른 괄호 문자열?
+    def check_correct(rotated_s):
+        stack = []
+        for letter in rotated_s:
+            if letter == "}" and stack and stack[-1] == "{":
+                stack.pop()
+            elif letter == "]" and stack and stack[-1] == "[":
+                stack.pop()
+            elif letter == ")" and stack and stack[-1] == "(":
+                stack.pop()
+            elif letter in ["{", "[", "("]:
+                stack.append(letter)
+            else:
+                return False
         
+        if (not stack):
+            return True
+        else:
+            return False
+                
+    answer = 0
+    for i in range(len(s)):
+        rotated_s = s[i:] + s[:i]
+        if check_correct(rotated_s):
+            answer += 1
+            
     return answer
