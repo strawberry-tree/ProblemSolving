b0c4a7505cdde1ff8d82f4e183a0e02fdd32c9c5
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 20:41:36 +0900

    [level 2] Title: 2 x n 타일링, Time: 13.04 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -1,3 +1,11 @@
 def solution(n):
-    answer = 0
-    return answer
+    tiles = [0] * (n + 1)
+    
+    for i in range(1, n + 1):
+        if i == 1:
+            tiles[i] = 1
+        elif i == 2:
+            tiles[i] = 2
+        else:
+            tiles[i] = (tiles[i-1] + tiles[i-2]) % 1000000007
+    return tiles[n]
