7ade0441a3bdf703b531a2c0fcd43037f4671da6
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 16:04:38 +0900

    [level 2] Title: 행렬의 곱셈, Time: 100.27 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,17 +1,11 @@
 def solution(arr1, arr2):
-    # (A x B) 행렬과 (B x C) 행렬의 곱은 (A X C)
-    n_rows = len(arr1)
-    n_mid = len(arr2)
-    n_cols = len(arr2[0])
-    answer = [[0] * n_cols for _ in range(n_rows)]
+    M = len(arr1)       # 최종 행렬 행
+    P = len(arr2)
+    N = len(arr2[0])    # 최종 행렬 열
+    answer = [[0] * N for _ in range(M)]
     
-    # 각 행과 각 열을 for문으로 불러옴
-    for i in range(n_rows):
-        for j in range(n_cols):
-            result = 0            
-            # 불러온 행, 열의 각 성분을 곱한 뒤 더함
-            for k in range(n_mid):
-                result += arr1[i][k] * arr2[k][j]
-            answer[i][j] = result
-
+    for i in range(M):
+        for j in range(N):
+            for k in range(P):
+                answer[i][j] += arr1[i][k] * arr2[k][j]
     return answer
