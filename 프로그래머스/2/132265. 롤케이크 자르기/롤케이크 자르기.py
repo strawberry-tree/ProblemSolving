d5727290b629a00d5cdb33324dd7f035d6183b15
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 20:07:48 +0900

    [level 2] Title: 롤케이크 자르기, Time: 898.03 ms, Memory: 49.5 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,20 +1,17 @@
-from collections import defaultdict
 from collections import Counter
-
-def solution(topping):
+def solution(topping):    
+    # 자를 수 있는 경우의수: idx(0) -> (1) 사이 -> idx (n-2)->(n-1) 사이
     answer = 0
+    first_piece = Counter(topping)
+    second_piece = set()
     
-    after_dict = Counter(topping)
-    before_set = set()
-    
-    # 토핑을 1개씩 after_dict에서 before_set로 넘기기
-    # 먹을 수 있는 토핑 수가 일치할 시 answer에 1 추가
-    # O(N)
     for t in topping:
-        after_dict[t] -= 1
-        if after_dict[t] == 0:
-            after_dict.pop(t)
-        before_set.add(t)
-        if len(before_set) == len(after_dict):
+        first_piece[t] -= 1 # 첫째 조각
+        if first_piece[t] == 0:
+            del first_piece[t]
+        second_piece.add(t) # 둘째 조각
+        
+        if len(first_piece) == len(second_piece):
             answer += 1
+        
     return answer
