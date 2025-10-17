e90b16b2a5e00c7638ee74ea4d3904abc3b29c7c
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 21:10:17 +0900

    [level 2] Title: 구명보트, Time: 9.60 ms, Memory: 9.53 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,3 +1,15 @@
 def solution(people, limit):
+    i = 0
+    j = len(people) - 1
     answer = 0
+    people.sort()
+    
+    while i <= j:
+        answer += 1
+        if i != j and people[i] + people[j] <= limit:
+            i += 1
+            j -= 1
+        else:
+            j -= 1
+    
     return answer
