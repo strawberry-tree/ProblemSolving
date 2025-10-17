bc18477a056d6451d9268366dc7fa2dcce2373f3
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 16:12:08 +0900

    [level 1] Title: 실패율, Time: 22.92 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,23 +1,18 @@
-def solution(N, stages):
-    users = [0] * (N + 2)         # 각 스테이지별 도달 사용자 수
-    failure = [0] * (N + 1)       # 각 스테이지별 실패율
-    
-    # stages 배열 순회하면서 users 배열 업데이트
-    for s in stages:
-        users[s] += 1
-    
-    # users 배열 순회하면서 실패율 계산
-    total = len(stages) # 전체 사용자 수
-    for idx in range(1, N + 1):
-        # 도달 유저가 없는 경우
-        if users[idx] == 0:
-            failure[idx] = 0
-        else:
-            failure[idx] = (users[idx]) / total
-            total -= users[idx]
+from collections import Counter
 
-    # 실패율 내림차순 기준으로 [1, 2, 3, .., N] 배열 정렬
-    answer = list(range(1, N + 1))
-    answer.sort(reverse=True, key=lambda x: failure[x])
+def solution(N, stages):
+    people_num = len(stages)    # 총 인원 수
+    stages_counter = Counter(stages)    # {스테이지번호: 실패인원 수}
+    failure_rate = {} # {스테이지번호: 실패율}
+    
+    for i in range(1, N + 1):
+        if people_num == 0:
+            failure_rate[i] = 0
+        else:
+            failure_rate[i] = stages_counter[i] / people_num
+            people_num -= stages_counter[i]
+        
+    answer = [i for i in range(1, N + 1)]
+    answer.sort(key = lambda x: failure_rate[x], reverse=True)
     
     return answer
