94a7693c418a2df615233396a270697dd2939948
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 20:57:28 +0900

    [level 2] Title: 땅따먹기, Time: 737.83 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,19 +1,14 @@
 def solution(land):
     dp = [[0] * 4 for _ in range(len(land))]
-    
-    for i in range(len(land)):
-        for j in range(4):
-            if i == 0:
-                dp[i][j] = land[i][j]
-            else:
-                max_prev = -float('inf')
-                for k in range(4):
-                    if k != j:
-                        max_prev = max(max_prev, dp[i - 1][k])
-                dp[i][j] = max_prev + land[i][j]
-    
-    return max(dp[-1])
-    
-    
 
-    return answer
+    # 첫행은 그대로
+    dp[0] = land[0]
+    
+    # 둘째 행부터
+    for i in range(len(dp)):
+        for j in range(4):
+            dp[i][0] = max(dp[i-1][1], dp[i-1][2], dp[i-1][3]) + land[i][0]
+            dp[i][1] = max(dp[i-1][0], dp[i-1][2], dp[i-1][3]) + land[i][1]
+            dp[i][2] = max(dp[i-1][0], dp[i-1][1], dp[i-1][3]) + land[i][2]
+            dp[i][3] = max(dp[i-1][0], dp[i-1][1], dp[i-1][2]) + land[i][3]
+    return max(dp[-1])
