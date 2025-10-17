44a5b5ffc06a5398c193c019a851485b1e4e1d62
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-17 17:04:41 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.32 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,30 +1,27 @@
 def solution(board, moves):
+    # 각 열을 스택으로 관리
     N = len(board)
+    stacks = [[] for _ in range(N)]
+    final_stack = []
     
-    stacks = [[] for _ in range(N)] # 뽑기 전 인형들의 스택의 리스트 
-    final_stack = []                # 뽑은 인형들의 스택
-    result = 0                      # 터뜨린 인형 수
-    
-    # 각 스택에 인형을 넣어줌 - O(N^2)
-    for col in range(N):
-        for row in range(N - 1, -1, -1):
-            doll = board[row][col]
-            if doll != 0:
-                stacks[col].append(doll)
-    
-    # 인형을 뽑고 최종 스택에 넣기 - O(M)
+    # j번째 열
+    for j in range(N):
+        # i번째 행, 스택이니까 역순
+        for i in range(N - 1, -1, -1):
+            if board[i][j] != 0:
+                stacks[j].append(board[i][j])
+        
+    answer = 0
+    # 인형뽑기
     for m in moves:
-        m -= 1
-        if stacks[m]:
-            doll = stacks[m].pop()
-        else:
+        if not stacks[m - 1]:
             continue
             
-        # 최상단 인형이랑 동일한 경우 pop, 아니면 push
-        if final_stack and final_stack[-1] == doll:
+        doll = stacks[m - 1].pop()
+        if final_stack and doll == final_stack[-1]:
             final_stack.pop()
-            result += 2
+            answer += 2
         else:
             final_stack.append(doll)
     
-    return result
+    return answer
