ee22e13fd4356f597ee8bb3b68344f1ecc89bf16
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-18 16:30:02 +0900

    [level 2] Title: 배달, Time: 1.57 ms, Memory: 9.84 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -2,34 +2,32 @@ import heapq
 from collections import defaultdict
 
 def solution(N, road, K):
-    # 인접 리스트로 그래프 구현
+    # 마을1, 마을2, 가중치 (양방통행 가능)
     graph = defaultdict(list)
-    min_weight = defaultdict(lambda: float('inf')) # 중복된 간선 제거를 위한 딕셔너리
-    for v1, v2, weight in road:
-        if weight < min_weight[(v1, v2)]:
-            min_weight[(v1, v2)] = weight
-    for (v1, v2), weight in min_weight.items():
-        graph[v1].append((v2, weight))
-        graph[v2].append((v1, weight))
-        
+    for a, b, c in road:
+        graph[a].append((b, c))
+        graph[b].append((a, c))
+
     # 다익스트라 알고리즘
-    distance = [float('inf')] * (N + 1)
-    distance[1] = 0
-    queue = [(0, 1)]
+    queue = []
+    dist = [float('inf')] * (N + 1)
+    dist[1] = 0
+    
+    # 가중치, 마을
+    heapq.heappush(queue, (0, 1))
     
     while queue:
-        dist, curr = heapq.heappop(queue)
-        if dist > distance[curr]:
+        cost, curr = heapq.heappop(queue)
+        if cost > dist[curr]:
             continue
-        for (adj, adj_dist) in graph[curr]:
-            new_dist = distance[curr] + adj_dist
-            if new_dist < distance[adj]:
-                distance[adj] = new_dist
-                heapq.heappush(queue, (new_dist, adj))
+        
+        for adj, adj_cost in graph[curr]:
+            new_cost = cost + adj_cost
+            if new_cost < dist[adj]:
+                dist[adj] = new_cost
+                heapq.heappush(queue, (new_cost, adj))
     
-    # K 이하 시간인 경우 계산
-    result = 0
-    for i in range(1, N + 1):
-        if distance[i] <= K:
-            result += 1
-    return result
+    # K 이하의 시간
+    answer = sum(d <= K for d in dist[1:])
+
+    return answer
