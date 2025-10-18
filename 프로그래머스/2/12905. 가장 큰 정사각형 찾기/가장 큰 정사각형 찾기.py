129cd524d8e82de710727dec8416b0549af27ef9
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-18 21:38:29 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 661.86 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -1,18 +1,7 @@
 def solution(board):
-    N, M = len(board), len(board[0])
-    dp = [[0] * (M + 1) for _ in range(N + 1)]
+    answer = 1234
 
-    # i행j열이 오른쪽 아래 칸인, 제일 큰 정사각형의 한 변 길이
-    for i in range(1, N + 1):
-        for j in range(1, M + 1):
-            if board[i-1][j-1] == 0:
-                dp[i][j] = 0
-            else:
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
 
-    answer = 0
-    for i in range(1, N + 1):
-        for j in range(1, M + 1):
-            answer = max(answer, dp[i][j])
-    
-    return answer ** 2
+    return answer
