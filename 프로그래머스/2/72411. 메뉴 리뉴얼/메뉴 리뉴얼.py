057071ae4a6bf1c0e11e986a53b683cbb5ff8080
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-18 15:06:55 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 5.53 ms, Memory: 9.41 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,27 @@
+from itertools import combinations
+from collections import Counter
+
+def solution(orders, course):
+    answer = []
+    for num_menu in course:
+        cmb_list = []
+        for order in orders:
+            for cmb in combinations(order, num_menu):
+                cmb_list.append(tuple(sorted(list(set(cmb)))))
+                
+        if not cmb_list:
+            continue
+            
+        cmb_counts = Counter(cmb_list)
+        
+        max_count = max(cmb_counts.values())
+        
+        if max_count < 2:
+            continue
+
+        for key, value in cmb_counts.items():
+            if value == max_count:
+                answer.append("".join(key))
+        
+    answer.sort()
+    return answer
