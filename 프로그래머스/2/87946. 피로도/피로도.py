16e6a263462b910cb25b9b79ed4be9a77089000a
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-18 17:25:00 +0900

    [level 2] Title: 피로도, Time: 87.99 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,16 +1,22 @@
-def enter(n, visited, k, dungeons):
-    # n: 지금까지 들어간 던전 수
-    answer = n
+# unvisited에 있는애들 방문가능, 현재체력 k, 현재방문 던전수 num
+def visit(unvisited, k, num, dungeons, results):
     
-    # i: 들어갈 던전
-    for i in range(len(dungeons)):
-        if k >= dungeons[i][0] and i not in visited:
-            visited.add(i)
-            answer = max(answer, enter(n + 1, visited, k - dungeons[i][1], dungeons))
-            visited.remove(i)
-    
-    return answer
+    for j in unvisited:
+        entry = dungeons[j][0]
+        minus = dungeons[j][1]
+        
+        # 입장이 가능한 경우
+        if entry <= k:
+            unvisited.remove(j)
+            visit(unvisited, k - minus, num + 1, dungeons, results)
+            unvisited.add(j)
+            
+    # 지금까지 방문한 던전수
+    results.append(num)
 
 def solution(k, dungeons):
-    visited = set()
-    return enter(0, visited, k, dungeons)
+    unvisited = set(i for i in range(len(dungeons)))
+    results = []
+    visit(unvisited, k, 0, dungeons, results)
+    return max(results)
+    
