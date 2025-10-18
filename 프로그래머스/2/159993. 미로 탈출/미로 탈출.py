f83b9c907357954b07d8d8228f7e4e0e08fa4f6e
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-18 15:20:23 +0900

    [level 2] Title: 미로 탈출, Time: 17.39 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,48 +1,46 @@
 from collections import deque
 
-# 시작위치 찾기
-def find_start(maps):
-    # O(N * M)
-    for i in range(len(maps)):
-        for j in range(len(maps[i])):
-            if maps[i][j] == "S":
-                return (i, j)
-
-# 특정 위치에서 목적지까지 최단 거리
-def find_target(maps, sx, sy, target):
-    n = len(maps)
-    m = len(maps[0])
-    dx = [-1, 0, 1, 0]
-    dy = [0, -1, 0, 1]
-
-    # 이동시간 저장 용도
-    grid = [[-1] * m for _ in range(n)]
-    grid[sx][sy] = 0
-        
-    queue = deque([(sx, sy)])
-    while queue:
-        x, y = queue.popleft()
-        if maps[x][y] == target:
-            return (grid[x][y], x, y)
-            
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m:
-                if maps[nx][ny] != "X" and grid[nx][ny] == -1:
-                    queue.append((nx, ny))
-                    grid[nx][ny] = grid[x][y] + 1
-    
-    return None
-
 def solution(maps):
-    sx, sy = find_start(maps)
-    result_l = find_target(maps, sx, sy, "L")
-    if result_l is None:
-        return -1
-    result_e = find_target(maps, result_l[1], result_l[2], "E")
-    if result_e is None:
-        return -1
-    
-    result = result_l[0] + result_e[0]
-    return result
+    def bfs(sx, sy, ex, ey):
+        dx = [-1, 0, 1, 0]
+        dy = [0, -1, 0, 1]
+        dist = [[-1] * len(maps[0]) for _ in range(len(maps))]
+
+        queue = deque([(sx, sy)])
+        dist[sx][sy] = 0
+        
+        while queue:
+            cx, cy = queue.popleft()
+            for i in range(4):
+                nx, ny = cx + dx[i], cy + dy[i]        
                 
+                if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]):
+                    if maps[nx][ny] != "X" and dist[nx][ny] == -1:
+                        queue.append((nx, ny))
+                        dist[nx][ny] = dist[cx][cy] + 1
+                        
+                        # 목적지 도착
+                        if nx == ex and ny == ey:
+                            return dist[nx][ny]
+        
+        return -1
+                        
+    # 시작점, 레버, 끝점 찾기
+    for i in range(len(maps)):
+        for j in range(len(maps[0])):
+            if maps[i][j] == "S":
+                sx, sy = i, j
+            elif maps[i][j] == "L":
+                lx, ly = i, j
+            elif maps[i][j] == "E":
+                ex, ey = i, j
+    
+
+    # 레버를 당기기 전
+    time1 = bfs(sx, sy, lx, ly)
+    time2 = bfs(lx, ly, ex, ey)
+    
+    if time1 == -1 or time2 == -1:
+        return -1
+    else:
+        return time1 + time2
