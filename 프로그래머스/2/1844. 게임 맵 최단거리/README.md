83f2ac90e082fa0c2dbd465ced3ab87d868a54bb
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2025-10-18 16:09:53 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.31 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 9.45 MB, 시간: 12.58 ms
+메모리: 9.35 MB, 시간: 14.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 15일 21:59:30
+2025년 10월 18일 16:12:20
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,33 @@
+# 전형적인 bfs문제
+
 from collections import deque
-      
-def solution(maps):                
-    N = len(maps)                           # 행의 수
-    M = len(maps[0])                        # 열의 수
+
+def solution(maps):
+    N = len(maps)
+    M = len(maps[0])
     
-    # 미방문 시 -1. 방문 시 distance.
-    visited = [[-1] * M for _ in range(N)]
-    visited[0][0] = 1                       # 시작 칸도 한 칸으로 침.
-    queue = deque([(0, 0)])
-    dx = [-1, 0, 1, 0]                      # 좌우 이동
-    dy = [0, -1, 0, 1]                      # 상하 이동
-
-    while queue:
-        cx, cy = queue.popleft()
-
-        # 상하좌우 탐색
-        for i in range(4):
-            nx, ny = cx + dx[i], cy + dy[i]
-
-            # 맵 안의 칸인가? 벽이 없는가? 방문하지 않은 칸인가?
-            if 0 <= nx < N and 0 <= ny < M and maps[nx][ny] == 1 and visited[nx][ny] == -1:
-                queue.append((nx, ny))
-                visited[nx][ny] = visited[cx][cy] + 1
-                
-    return visited[-1][-1]
+    def bfs(sx, sy):
+        dx = [-1, 0, 1, 0]
+        dy = [0, -1, 0, 1]
+        
+        dist = [[-1] * M for _ in range(N)]
+        queue = deque([(sx, sy)])
+        dist[sx][sy] = 1
+        
+        while queue:
+            cx, cy = queue.popleft()
+            for i in range(4):
+                nx, ny = cx + dx[i], cy + dy[i]
+                if 0 <= nx < N and 0 <= ny < M:
+                    if maps[nx][ny] == 1 and dist[nx][ny] == -1:
+                        dist[nx][ny] = dist[cx][cy] + 1
+                        
+                        if nx == N - 1 and ny == M - 1:
+                            return dist[nx][ny]
+                        
+                        queue.append((nx, ny))
+        
+        return -1
+                        
+    
+    return bfs(0, 0)
