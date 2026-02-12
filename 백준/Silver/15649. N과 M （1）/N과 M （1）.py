025b45bc81dbd7d0e6cbd62c3241b14de343030a
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2026-02-12 22:47:28 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,21 +1,20 @@
+import sys
+sys.setrecursionlimit(10**9)
 N, M = map(int, input().split())
 
-picked = [False] * (N + 1)
-curr = []
+chosen = [False] * (N + 1)
 
-
-def pick(i):
-    if i > M:
-        for c in curr:
-            print(c, end=" ")
-        print()
-    else:
-        for k in range(1, N + 1):
-            if not picked[k]:
-                picked[k] = True
-                curr.append(k)
-                pick(i + 1)
-                curr.pop()
-                picked[k] = False
-        
-pick(1)
+def perm(curr, chosen):
+    if len(curr) >= M:
+        print(*curr)
+        return
+    
+    for i in range(1, N + 1):
+        if not chosen[i]:
+            chosen[i] = True
+            curr.append(i)
+            perm(curr, chosen)
+            curr.pop()
+            chosen[i] = False
+            
+perm([], chosen)
