9b764c4e1a1e31e132949881280d5c5b717ac87e
송상록 <157905465+strawberry-tree@users.noreply.github.com>
2026-02-12 23:24:15 +0900

    [Silver II] Title: DFS와 BFS, Time: 68 ms, Memory: 35676 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,56 +1,44 @@
-from collections import deque
+from collections import defaultdict, deque
 import sys
 
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
+sys.setrecursionlimit(10**9)
 
 N, M, V = map(int, input().split())
-graph = [[] for _ in range(N+1)]
+graph = defaultdict(list)
 
 for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    start, end = map(int, input().split())
+    graph[start].append(end)
+    graph[end].append(start)
 
-stack = []
-unvisited_s = [i for i in range(1, N+1)]
-result_s = []
-queue = deque()
-unvisited_q = [i for i in range(1, N+1)]
-result_q = []
+# 정점 번호 작은 순으로 방문 위함
+for i in range(1, N + 1):
+    graph[i].sort()
+    
+visited_dfs = [False] * (N + 1)
+visited_bfs = [False] * (N + 1)
 
-stack.append(V)
-unvisited_s.remove(V)
+def dfs(curr):
+    print(curr, end=" ")
+    visited_dfs[curr] = True
+    for adj in graph[curr]:
+        if not visited_dfs[adj]:
+            dfs(adj)
+            
+def bfs(start):
+    visited_bfs[start] = True
+    queue = deque([start])
+    
+    while queue:
+        curr = queue.popleft()
+        print(curr, end=" ")
+        for adj in graph[curr]:
+            if not visited_bfs[adj]:
+                visited_bfs[adj] = True
+                queue.append(adj)
+                
 
-while stack:
-    p = stack[-1]
-    if p not in result_s:
-        result_s.append(p)
-    dt = None
-    for d in sorted(graph[p]):
-        if d in unvisited_s:
-            dt = d
-            break
-    if dt:
-        stack.append(dt)
-        unvisited_s.remove(dt)
-    else:
-        stack.pop()
-
-
-queue.append(V)
-unvisited_q.remove(V)
-
-while queue:
-    p = queue.popleft()
-    result_q.append(p)
-    for d in sorted(graph[p]):
-        if d in unvisited_q:
-            queue.append(d)
-            unvisited_q.remove(d)
-
-print(' '.join(map(str, result_s)))
-print(' '.join(map(str, result_q)))
-
-
-
-
+dfs(V)
+print()
+bfs(V)
